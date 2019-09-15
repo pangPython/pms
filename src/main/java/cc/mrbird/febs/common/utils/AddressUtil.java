bda3ef445a0384229a77031e02301d62a5f4c8de
mrbird <wuyouzhuguli@gmail.com>
2019-09-15 09:37:20 +0800

    修复地址工具类在本地开发环境下报错问题

--- src/main/java/cc/mrbird/febs/common/utils/AddressUtil.java
+++ src/main/java/cc/mrbird/febs/common/utils/AddressUtil.java
@@ -11,8 +11,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.util.Objects;
 
 /**
  * @author MrBird
@@ -30,7 +30,10 @@ public class AddressUtil {
                 String tmpDir = System.getProperties().getProperty("java.io.tmpdir");
                 dbPath = tmpDir + "ip.db";
                 file = new File(dbPath);
-                FileUtils.copyInputStreamToFile(Objects.requireNonNull(AddressUtil.class.getClassLoader().getResourceAsStream("classpath:ip2region/ip2region.db")), file);
+                InputStream resourceAsStream = AddressUtil.class.getClassLoader().getResourceAsStream("classpath:ip2region/ip2region.db");
+                if (resourceAsStream != null) {
+                    FileUtils.copyInputStreamToFile(resourceAsStream, file);
+                }
             }
             DbConfig config = new DbConfig();
             searcher = new DbSearcher(config, file.getPath());
@@ -39,11 +42,10 @@ public class AddressUtil {
                 log.error("Error: Invalid ip address");
             }
             DataBlock dataBlock = (DataBlock) method.invoke(searcher, ip);
-            if (dataBlock == null)
-                return StringUtils.EMPTY;
             return dataBlock.getRegion();
         } catch (Exception e) {
-            log.error("获取地址信息异常", e);
+            log.error("获取地址信息异常，{}", e.getMessage());
+            return StringUtils.EMPTY;
         } finally {
             if (searcher != null) {
                 try {
@@ -53,7 +55,5 @@ public class AddressUtil {
                 }
             }
         }
-        return StringUtils.EMPTY;
     }
-
 }
